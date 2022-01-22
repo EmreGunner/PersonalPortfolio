7e82baf168ae3593809e6e1a0008d7ac2eb620de
ali-sabry <th3man1998@gmail.com>
2022-01-22 23:27:12 +0200

    Initial empty commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# developer portfolio
+developer Personal portfolio bootstrap template for developer.
+```
+```
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Preview](#preview)
+
+```
+```
+
+# General info
+
+This Template is Re_building from exist project from themeforest, this project is for training only.
+
+The purpose from rebuilding this template is to for practicing. & to be ready for real project.
+
+```
+```
+## Technologies
+Project is created with:
+* Html       : Version_5.
+* CSS        : Version_3.
+* JavaScript : Es6.
+* Bootstrap  : Version_5.
+* Fontawsome : Version_5.
+
+```
+```
+
+## Preview Link:
+
+Use the link to preview [developer personal portfolio](https://github.com/ali-sabry/developer_personal_portfolio).
+
+```bash
+Note: This Template Is For Presentation Only.
+```
+
+
+
+
+## License
+This template is not for commercial Use if you need this template go to the original one [inbio](https://themeforest.net/item/inbio-one-page-personal-portfolio-template/33188244) .
+
+```
+
+```
---
+++ js/main.js
@@ -0,0 +1,136 @@
+//====== Global Vars
+    let Lg_navbar      = document.querySelector("nav");
+    let top_bar_navbar = document.querySelector(".scrolling-navbar");
+    let side_nav       = document.querySelector(".offcanvas");
+//===== All Panels Buttons In Website For Using In Shuffle
+    // Resume Section Panel Buttons
+    let resume_tabs = document.querySelectorAll(".resume .panel-dad ul li a");
+    let resume_cont = document.querySelectorAll(".resume .content");
+    // Clients Section Panel Buttons
+    let clients_tabs = document.querySelectorAll( ".clients .left-side-buttons ul li a");
+    let clients_cont = document.querySelectorAll(".clients .content");
+    // Pricing  Section Panel Buttons
+    let pricing_tabs = document.querySelectorAll(".pricing-panel ul li a");
+    let pricing_cont = document.querySelectorAll(".pricing .content");
+
+//===== All Effects On Scroll Window Function
+    function scrolling_site() {
+        window.onscroll = () => {
+            // Set Navbar Background Effect On Scroll
+            if (window.pageYOffset > 200) {
+                document.querySelector(".scrolling-navbar").classList.add("scrolling-nav");
+            } else {
+                document
+                .querySelector(".scrolling-navbar")
+                .classList.remove("scrolling-nav");
+            }
+        
+            // Show Button Go Top On Scroll
+            if (window.pageYOffset > 800) {
+                document.querySelector(".to-top-btn").style.opacity = 1;
+            } else {
+                document.querySelector(".to-top-btn").style.opacity = 0;
+            }
+        };  
+    };
+
+//===== Go To Top On Click Button
+    function to_top() {
+            let to_top = document.querySelector(".to-top-btn");
+            to_top.onclick = () => {
+                window.window.scrollTo(0, 0);
+            };
+    };
+
+
+//=========== Side Navbar Custmiztion
+    function customization_menu() {
+        document.body.onclick = () => {
+            if (side_nav.style.visibility == "visible") {
+                document.body.style.overflow = "auto";
+                document.body.style.paddingRight = 0;
+                top_bar_navbar.style.paddingRight = 0;
+                side_nav.style.maxWidth = `${90}%`;
+            } else {
+                return false;
+            }
+        };
+    };
+
+
+//========== Shuffle Content 
+    function shuffling() {
+        //===== Function Active Button On Clicked
+            function shuffle(tabs, content) {
+                let theTabs = Array.from(tabs);
+                let theContent = Array.from(content);
+        
+                //===== Active Button On Clicked
+                theTabs.forEach((tab) => {
+                    tab.addEventListener("click", (el) => {
+                    // Prevent Button Default Job
+                    el.preventDefault();
+        
+                    // Remove Active Class
+                    theTabs.forEach((btn) => {
+                        btn.classList.remove("active");
+                    });
+        
+                    // Add Class Active
+                    el.currentTarget.classList.add("active");
+        
+                    // Hide Div Contnet
+                    theContent.forEach((cont) => {
+                        cont.style.display = "none";
+                    });
+        
+                    // Show Target Div
+                    document.querySelector(el.currentTarget.dataset.content).style.display =
+                        "block";
+                    });
+                });
+            }
+        
+        //========== Run Function Active Tabs & Target Content
+            shuffle(resume_tabs, resume_cont);
+            shuffle(clients_tabs, clients_cont);
+            shuffle(pricing_tabs, pricing_cont);
+    }  
+    
+//============ Body Custmization
+    function customization_body() {
+        // Check Upper Navbar Inner Height
+        if (Lg_navbar.clientHeight > 10) {
+            document.body.style.paddingTop = `${Lg_navbar.clientHeight}px`;
+        } else if (top_bar_navbar.clientHeight > 10) {
+            document.body.style.paddingTop = `${top_bar_navbar.clientHeight}px`;
+        } else {
+            document.body.style.paddingTop = 0;
+        }
+    }   
+
+//======== Set Padding Top For Body & Padding Right For Top_Bar_Navbar
+    window.onload = ()=> {
+        top_bar_navbar.style.paddingRight = 0;
+
+        // Run Custimazition Body Function
+        customization_body();
+
+        // Run Scrolling Function
+        scrolling_site();
+
+        // Run Go To Top Function
+        to_top();
+
+        // Run Custimazition Side Menu Function
+        customization_menu();
+
+        // Run Shuffle Content
+        shuffling();
+      
+    };
+
+        
+
+
+
